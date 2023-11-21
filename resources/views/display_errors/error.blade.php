627117e82a7222f0a56b9103ae1d8e2bfbdb9100
RyoHatano <ryohatano0809@gmail.com>
2023-11-21 12:44:17 +0900

    bootstrapを使ったCSS

--- resources/views/display_errors/error.blade.php
+++ resources/views/display_errors/error.blade.php
@@ -1,7 +1,7 @@
 @if ($errors->any())
-    <ul class="alert">
+    <div class="alert alert-danger" role="alert">
         @foreach ($errors->all() as $error)
             <li>{{ $error }}</li>
         @endforeach
-    </ul>
+    </div>
 @endif
--- resources/views/templates/forms.blade.php
+++ resources/views/templates/forms.blade.php
@@ -1,11 +1,19 @@
 @csrf 
-<dl class="form-list">
-    <dt>内容</dt>
-    <dd><textarea name="content" rows="5">{{ old('content', $schedule->content) }}</textarea></dd>
-    <dt>場所</dt>
-    <dd><textarea name="place" rows="5">{{ old('place', $schedule->place) }}</textarea></dd>
-    <dt>開始日時</dt>
-    <dd><input type="datetime-local" name="start_time" value="{{ old('start_time', $schedule->start_time) }}"></dd>
-    <dt>終了日時</dt>
-    <dd><input type="datetime-local" name="end_time" value="{{ old('end_time', $schedule->end_time) }}"></dd>
-</dl>
+<div class="schedule-create">
+    <div class="mb-3">
+    <label class="form-label">内容</label>
+    <dd><textarea class="form-control" name="content" rows="5">{{ old('content', $schedule->content) }}</textarea></dd>
+    </div>
+    <div class="mb-3">
+    <label class="form-label">場所</label>
+    <dd><textarea class="form-control" name="place" rows="5">{{ old('place', $schedule->place) }}</textarea></dd>
+    </div>
+    <div class="mb-3">
+    <label class="form-label">開始日時</label>
+    <dd><input class="form-control" type="datetime-local" name="start_time" value="{{ old('start_time', $schedule->start_time) }}"></dd>
+    </div>
+    <div class="mb-3">
+    <label class="form-label">終了日時</label>
+    <dd><input class="form-control" type="datetime-local" name="end_time" value="{{ old('end_time', $schedule->end_time) }}"></dd>
+    </div>
+</div>
